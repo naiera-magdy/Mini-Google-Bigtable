dfacb629df33bfb2a9504abe9b50f11007262caf
naiera-magdy <44544070+naiera-magdy@users.noreply.github.com>
2021-06-03 12:02:19 +0200

    Completed CRUD operations of the client

--- controllers/showController.js
+++ controllers/showController.js
@@ -1,11 +1,73 @@
 const Show = require('../models/showModel');
 
-exports.setCells = function() {};
-exports.deleteCells = function() {};
-exports.deleteRow = function() {};
-exports.addRow = function() {};
-exports.readRow = async function(id) {
-  const data = await Show.find({ show_id: id });
+exports.setCells = async function(data) {
   const socket = this;
-  socket.emit('readRows', data);
+  try {
+    const id = data.show_id;
+    // eslint-disable-next-line dot-notation
+    delete data['show_id'];
+    // console.log(data);
+    const res = await Show.updateOne({ show_id: id }, data);
+    socket.emit('setCells', res);
+  } catch (err) {
+    console.log(err);
+    socket.emit('setCells', "Error can't complete the query");
+  }
+};
+
+exports.deleteCells = async function(data) {
+  const socket = this;
+  try {
+    const id = data.show_id;
+    // eslint-disable-next-line dot-notation
+    delete data['show_id'];
+    const fields = {};
+    // eslint-disable-next-line no-restricted-syntax
+    for (const field of data.fields) {
+      fields[field] = 1;
+    }
+    // console.log(fields);
+
+    const res = await Show.updateOne({ show_id: id }, { $unset: fields });
+    socket.emit('deleteCells', res);
+  } catch (err) {
+    console.log(err);
+    socket.emit('deleteCells', "Error can't complete the query");
+  }
+};
+
+exports.deleteRow = async function(ids) {
+  const socket = this;
+  try {
+    const res = await Show.deleteMany({ show_id: ids });
+    socket.emit('deleteRow', res);
+  } catch (err) {
+    console.log(err);
+    socket.emit('deleteRow', "Error can't complete the query");
+  }
+};
+
+exports.addRow = async function(data) {
+  const socket = this;
+  try {
+    const res = await Show.create(data);
+    socket.emit('addRow', res);
+  } catch (err) {
+    console.log(err);
+    socket.emit(
+      'addRow',
+      'Duplicate key error please make sure to provide a unique id'
+    );
+  }
+};
+
+exports.readRow = async function(id) {
+  const socket = this;
+  try {
+    const data = await Show.find({ show_id: id });
+    socket.emit('readRows', data);
+  } catch (err) {
+    console.log(err);
+    socket.emit('setCells', "Error can't complete the query");
+  }
 };
