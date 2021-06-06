4521c1487a350a71e7b29c97e00bb68c4071c719
Happysword <45149700+Happysword@users.noreply.github.com>
2021-06-06 23:34:42 +0200

    Added Load Balance Test Case

--- Client/Main.js
+++ Client/Main.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+
 $(document).ready(function () {
   // Socket Connections
 
@@ -184,10 +185,9 @@ $(document).ready(function () {
       console.log(objectToSend);
       if (NUMBER_OF_SERVERS === 2) {
         console.log("Sending");
-        let socket =
-          rowkey[0] >= Tabletcache[0].Start && rowkey[0] <= Tabletcache[0].End
-            ? TabletServer1
-            : TabletServer2;
+        let socket = rowkey <= Tabletcache[0].End
+          ? TabletServer1
+          : TabletServer2;
         socket.emit('show:Set', objectToSend);
         console.log("Sent");
       } else {
@@ -212,7 +212,7 @@ $(document).ready(function () {
 
       if (NUMBER_OF_SERVERS === 2) {
         let socket =
-          rowkey[0] >= Tabletcache[0].Start && rowkey[0] <= Tabletcache[0].End
+          rowkey >= rowkey <= Tabletcache[0].End
             ? TabletServer1
             : TabletServer2;
         socket.emit('show:DeleteCells', objectToSend);
@@ -232,8 +232,7 @@ $(document).ready(function () {
 
         if (NUMBER_OF_SERVERS === 2) {
           if (
-            $(this).val()[0] >= Tabletcache[0].Start &&
-            $(this).val()[0] <= Tabletcache[0].End
+            $(this).val() <= Tabletcache[0].End
           ) {
             tabletArray1.push($(this).val());
           } else tabletArray2.push($(this).val());
@@ -279,7 +278,7 @@ $(document).ready(function () {
 
       if (NUMBER_OF_SERVERS === 2) {
         let socket =
-          rowkey[0] >= Tabletcache[0].Start && rowkey[0] <= Tabletcache[0].End
+          rowkey <= Tabletcache[0].End
             ? TabletServer1
             : TabletServer2;
         socket.emit('show:AddRow', objectToSend);
@@ -299,8 +298,7 @@ $(document).ready(function () {
       $('#readrow .rowkey').each(function (el) {
         if (NUMBER_OF_SERVERS === 2) {
           if (
-            $(this).val()[0] >= Tabletcache[0].Start &&
-            $(this).val()[0] <= Tabletcache[0].End
+            $(this).val() <= Tabletcache[0].End
           ) {
             tabletArray1.push($(this).val());
           } else tabletArray2.push($(this).val());
@@ -376,4 +374,51 @@ $(document).ready(function () {
     $('#response').css('display', 'none');
     $('#main').css('display', 'flex');
   });
+
+  // Event On button of Test Case
+  $('#deletetestcase').click(TestCaseDeleteLoadBalance);
+
+
+
+  // Test Case: Delete Alot 
+  function TestCaseDeleteLoadBalance() {
+    let tabletArray1 = [];
+    let tabletArray2 = [];
+    let ArrayToSend = [];
+    testBalanceArray.forEach(function (el) {
+      if (NUMBER_OF_SERVERS === 2) {
+        if (
+
+          $(this).val() <= Tabletcache[0].End
+        ) {
+          tabletArray1.push($(this).val());
+        } else tabletArray2.push($(this).val());
+      } else {
+        tabletArray1.push($(this).val());
+      }
+    });
+
+    if (NUMBER_OF_SERVERS === 2) {
+
+      if (tabletArray1.length)
+        TabletServer1.emit('show:ReadRows', tabletArray1);
+      if (tabletArray2.length)
+        TabletServer2.emit('show:ReadRows', tabletArray2);
+
+      ArrayToSend = [...tabletArray1, ...tabletArray2];
+    } else {
+
+      if (tabletArray1.length) {
+        ArrayToSend = [...tabletArray1];
+        TabletServer1.emit('show:ReadRows', tabletArray1);
+      }
+    }
+
+    // Log to Master
+    log = `Sent Delete Rows of keys ${ArrayToSend.toString()}`;
+    MasterServer.emit("logs", log);
+  }
+
 });
+
+var testBalanceArray = ["A 2nd Chance", "A Billion Colour Story", "A Bridge Too Far", "A Choo", "A Christmas Special: Miraculous: Tales of Ladybug & Cat Noir", "A Cinderella Story: Christmas Wish", "A Fall from Grace", "A Family Reunion Christmas", "A Futile and Stupid Gesture", "A Go! Go! Cory Carson Summer Camp", "A Good Wife", "A Haunting at Silver Falls: The Return", "A Kid from Coney Island", "A Land Imagined", "A Little Chaos", "A Little Thing Called First Love", "A Love Song for Latasha", "A Mission in an Old Movie", "A Murder in the Park", "A Patch of Fog", "A Plastic Ocean", "A Princess for Christmas", "A Second Chance", "A Series of Unfortunate Events", "A Silent Voice", "A Stoning in Fulham County", "A Sun", "A Thousand Goodnights", "A Trip to Jamaica", "A Very Country Christmas", "A Wednesday", "A Week in Watts", "A Whisker Away", "A Young Doctor's Notebook and Other Stories", "A.M.I.", "Aagey Se Right", "Aapla Manus", "Aashayein", "Abby Hatcher", "ABCD: Any Body Can Dance", "Abdullah, The Final Witness", "About Time", "Abstract: The Art of Design", "Acapulco La vida va", "Across the Universe", "Action Replayy", "Adأ؛", "Adam Ruins Everything", "Adam: His Song Continues", "Aditi Mittal: Things They Wouldn't Let Me Say", "Adrishya", "Afonso Padilha: Classless", "After Life", "After Maria", "Aftermath", "Against the Tide", "Age of Glory", "Agent Raghav", "Aggretsuko: We Wish You a Metal Christmas", "Ai Weiwei: Never Sorry", "Ainu Mosir", "Aitraaz", "Ajaibnya Cinta", "AJIN: Demi-Human", "Ajji", "Al acecho", "Alakada Reloaded", "Albert Pinto Ko Gussa Kyun Aata Hai?", "Alex Fernأ،ndez: The Best Comedian in the World", "Alexa & Katie", "Ali & Alia", "Ali Wong: Hard Knock Wife", "Alias JJ, la celebridad del mal", "Alien Contact: Outer Space", "Alien TV", "Aliens Ate My Homework", "All American", "All Dogs Go to Heaven", "All Hail King Julien: Exiled", "All In My Family", "All of You", "All the Freckles in the World", "All's Well, End's Well (2009)", "Alone", "Alpha and Omega 2: A Howl-iday Adventure", "Alpha and Omega: The Legend of the Saw Tooth Cave", "Altered Carbon: Resleeved", "Always Be My Maybe", "Amar", "Amateur", "Amelia: A Tale of Two Sisters", "American Beauty", "American Experience: Ruby Ridge", "American Factory", "American Honey", "American Horror Story", "American Masters: Ted Williams", "American Son", "Amit Tandon: Family Tandoncies", "Amy", "An American Tail: Fievel Goes West", "An American Tail: The Treasures of Manhattan Island", "An Hour and a Half", "An Interview with God", "An Upper Egyptian", "Anchor Baby", "Ancient Aliens", "Andhadhun", "Anesthesia", "Angel Beats!", "Angela's Christmas Wish", "Anger Management", "Angry Indian Goddesses", "Animal Crackers", "Anitta: Made In Honأ³rio", "Ankhon Dekhi", "Anohana: The Flower We Saw That Day", "Anon", "Ant-Man and the Wasp", "Anthony Bourdain: Parts Unknown", "Anthony Kaun Hai?", "ه؟چè€…مƒڈمƒƒمƒˆمƒھمپڈم‚“", "Apache: The Life of Carlos Tevez", "Apostle", "Aram, Aram", "Archibald's Next Big Thing", "Ares", "Argon", "Ari Eldjأ،rn: Pardon My Icelandic", "ARQ", "Arsenio Hall: Smart & Classy", "Article 15", "Asees", "Ashes of Love", "Asperger's Are Us", "Astronomy Club: The Sketch Show", "Asura: The City of Madness", "Athlete A", "Atlantics", "Attack on Titan", "Aunty Donna's Big Ol' House of Fun", "Aussie Gold Hunters", "Autohead", "Autumn's Concerto", "Avatar: The Last Airbender", "Avicii: True Stories", "Awake: The Million Dollar Game", "Ayana", "Aziz Ansari Live at Madison Square Garden", "Aziz Ansari: Buried Alive", "Baadshaho"];
